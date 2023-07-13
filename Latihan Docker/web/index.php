5975d4c8e48b29065a165cab936ad734f174cc42
DewaGedeBhaskaraPramudya <114656764+DewaGedeBhaskaraPramudya@users.noreply.github.com>
2023-07-13 23:41:52 +0800

    Add files via upload

---
+++ Latihan Docker/web/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contoh Web Sederhana</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f2f2f2;
+            margin: 0;
+            padding: 0;
+        }
+
+        .container {
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 20px;
+        }
+
+        h1 {
+            color: #333;
+            text-align: center;
+        }
+
+        .profile {
+            background-color: #fff;
+            border-radius: 5px;
+            padding: 20px;
+            margin-top: 30px;
+        }
+
+        .profile p {
+            margin: 0;
+        }
+
+        .profile h2 {
+            color: #333;
+            margin-bottom: 10px;
+        }
+
+        .profile label {
+            font-weight: bold;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Profil Saya</h1>
+        <div class="profile">
+            <h2>Bhaskara Pramudya Web</h2>
+            <p><label>Nama:</label> Dewa Gede Bhaskara Pramudya </p>
+            <p><label>NIM:</label> 2115101023 </p>
+            <p><label>Kelas:</label> IKI</p>
+        </div>
+    </div>
+</body>
+</html>
